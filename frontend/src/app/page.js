c2f4cdb94363230d470f75de8e08f7272b6b566a
neels22 <indraneelsarode@gmail.com>
2025-05-10 12:11:14 -0700

    deleted the entire frontend

--- frontend/src/app/page.js
+++
@@ -1,8 +0,0 @@
-// app/page.js
-import { redirect } from 'next/navigation';
-
-export default function Home() {
-  // This will trigger a server-side redirect to the /login route
-  redirect('/login');
-  // Since redirect() never returns, you don't need to return any JSX.
-}
