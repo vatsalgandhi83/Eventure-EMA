ca906f492f4f8296b8cb4d3f4d5a3a9f8ec4be7c
vatsalgandhi83 <vats.gandhi.83@gmail.com>
2025-05-11 15:51:41 -0700

    Event by user id update

--- backend/src/main/java/com/eventure/events/model/BookingDetails.java
+++ backend/src/main/java/com/eventure/events/model/BookingDetails.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 
+@Builder
 @Data
 @AllArgsConstructor
 @Document(collection = "BookingDetails")
